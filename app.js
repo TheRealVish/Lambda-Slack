33894e5aa8e3005f4e1b917ef9e5415278eeb910
TheRealVish <vish@goldsmithsconsulting.com>
2020-03-08 00:03:35 +0000

    env variable

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 'use strict'
 //lambda to post messages on to slack channel
 const https = require('https');
-
 exports.handler = (event, context, callback) => {
+  const xpath = process.env.XPATH_VALUE
   const payload = JSON.stringify({
     text: `Message sent by ${event.name} :\n ${event.message}`,
   })
-  
   const options = {
     hostname: "hooks.slack.com",
     method: "POST",
-    path: "/services/xx/xx/xx",
+    path: `${xpath}`
   }
-
   const req = https.request(options,
       (res) => res.on("data", () => callback(null, "OK")))
   req.on("error", (error) => callback(JSON.stringify(error)))
   req.write(payload)
   req.end()
 }
-
-/*
+/*const xpath = process.env.XPATH_VALUE
+console.log(xpath)
 const payload = JSON.stringify({text: `BOOM!`})
 console.log(payload)
 const options = {
   hostname: "hooks.slack.com",
   method: "POST",
-  path: "/services/xx/xx/xx"
+  path: `${xpath}`
 }
 const req = https.request(options, (res)=>{
   console.log('statusCode:',res.statusCode)
